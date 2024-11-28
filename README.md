9346021cc02221c9492081bb858554309c2d2503
11278515 <uyen.truong@autozone.com>
2024-11-28 13:46:44 -0600

    convert zones to counties

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # magnum_opus
 This is a repository for analyzing the hazardous weather across the State of Alabama
+
+# create virtual environment to use python in VSCode
+py -m venv myvenv
+.\myvenv\Scripts\activate
+pip install <package>
+
+# to read excel files w/ Pandas:
+pip install openpyxl
--- dataCleanser.py
+++ dataCleanser.py
@@ -4,7 +4,6 @@ import numpy as np
 cyclones = pd.read_csv("TROPICAL_DEPRESSIONS_STORMS_AND_HURRICANES_2000_2024_ALL.csv")
 dfCyclones = pd.DataFrame(cyclones)
 dfCyclones.drop_duplicates()
-print(dfCyclones.columns)
 
 winds = pd.read_csv("TORNADOES_AND_THUNDERSTORM_WINDS_2000_2024_ALL.csv")
 dfWinds = pd.DataFrame(winds)
@@ -16,16 +15,16 @@ dfSeaLevels.drop_duplicates()
 
 
 #This is a function to construct a summary table for count of events, # of deaths, injuries, and property damages
-def createSummaryTable(df_raw_data, event_type_list):
+def createSummaryTable(df_raw_data, event_type_list,loc_type,FIPS_type):
     dfSummary = pd.DataFrame()
-    dfSummary["FIPS"] = df_raw_data['CZ_FIPS']
-    dfSummary['Counties'] = df_raw_data['CZ_NAME_STR']
+    dfSummary[FIPS_type] = df_raw_data['CZ_FIPS']
+    dfSummary[loc_type] = df_raw_data['CZ_NAME_STR']
     dfSummary.drop_duplicates(inplace=True)
     metrics = {
-        "Count": lambda df, row, event: len(df[(df["CZ_FIPS"] == row["FIPS"]) & (df["EVENT_TYPE"] == event)]),
-        "Deaths": lambda df, row, event: df[(df["CZ_FIPS"] == row["FIPS"]) & (df["EVENT_TYPE"] == event)]['DEATHS_DIRECT'].sum(),
-        "Injuries": lambda df, row, event: df[(df["CZ_FIPS"] == row["FIPS"]) & (df["EVENT_TYPE"] == event)]['INJURIES_DIRECT'].sum(),
-        "PropertyDamages": lambda df, row, event: df[(df["CZ_FIPS"] == row["FIPS"]) & (df["EVENT_TYPE"] == event)]['DAMAGE_PROPERTY_NUM'].sum(),
+        "Count": lambda df, row, event: len(df[(df["CZ_FIPS"] == row[FIPS_type]) & (df["EVENT_TYPE"] == event)]),
+        "Deaths": lambda df, row, event: df[(df["CZ_FIPS"] == row[FIPS_type]) & (df["EVENT_TYPE"] == event)]['DEATHS_DIRECT'].sum(),
+        "Injuries": lambda df, row, event: df[(df["CZ_FIPS"] == row[FIPS_type]) & (df["EVENT_TYPE"] == event)]['INJURIES_DIRECT'].sum(),
+        "PropertyDamages": lambda df, row, event: df[(df["CZ_FIPS"] == row[FIPS_type]) & (df["EVENT_TYPE"] == event)]['DAMAGE_PROPERTY_NUM'].sum(),
     }
 
     # Initialize empty dictionaries to store results for each metric
@@ -46,16 +45,16 @@ def createSummaryTable(df_raw_data, event_type_list):
     return dfSummary
 
 
-# CYCLONES (county):
+# CYCLONES (zone):
 cyclone_event_types = ["Hurricane", "Tropical Storm", "Tropical Depression"]
-dfCyclonesSummary = createSummaryTable(dfCyclones,cyclone_event_types)
-print(dfCyclonesSummary)
+dfCyclonesSummary = createSummaryTable(dfCyclones,cyclone_event_types,"Zone","Zone_FIPS")
+# print(dfCyclonesSummary.columns)
 
 
-# WINDS (zone):
+# WINDS (county):
 wind_event_types = ["Tornado", "Thunderstorm Wind"]
-dfWindsSummary= createSummaryTable(dfWinds, wind_event_types)
-print(dfWindsSummary)
+dfWindsSummary= createSummaryTable(dfWinds, wind_event_types,"County","County_FIPS")
+# print(dfWindsSummary.columns)
 #Including Magnitudes (EF scale for tornadoes, and knotts or Beaufort scale for thunderstorm winds)
 
 # HAIL (zone):
@@ -63,7 +62,34 @@ print(dfWindsSummary)
 
 # FLOOD (county):
 
-# SEA LEVELS (county):
+# SEA LEVELS (zone):
 seaLevel_event_types = ["Coastal Flood","Storm Surge/Tide"]
-dfSeaLevelsSummary= createSummaryTable(dfSeaLevels, seaLevel_event_types)
+dfSeaLevelsSummary= createSummaryTable(dfSeaLevels, seaLevel_event_types,"Zone","Zone_FIPS")
+# print(dfSeaLevelsSummary)
+
+# ZONE TO COUNTY CONVERSION
+counties = pd.read_excel("county_zone_correl.xlsx")
+dfCounties = pd.DataFrame(counties)
+print(dfCounties.columns)
+dfZoneCounty = dfCounties[dfCounties['STATE']=='AL'][['ZONE','FIPS','COUNTY']]
+dfZoneCounty = dfZoneCounty.reset_index(drop=True)
+
+# get the county part of the full FIPS codes:
+county_FIPS = []
+for _, row in dfZoneCounty.iterrows():
+    county_FIPS.append(str(row["FIPS"])[-3:].lstrip('0'))
+
+dfZoneCounty['County_FIPS'] = county_FIPS
+dfZoneCounty = dfZoneCounty.drop('FIPS',axis=1)
+print(dfZoneCounty)
+
+# Assign the corresponding County to the zones- For Sea Levels
+dfSeaLevelsSummary = pd.merge(dfSeaLevelsSummary, dfZoneCounty[["ZONE","County_FIPS","COUNTY"]], left_on='Zone_FIPS', right_on='ZONE', how='left')
 print(dfSeaLevelsSummary)
+
+pd.set_option('display.max_rows', None)
+dfCyclonesSummary = pd.merge(dfCyclonesSummary, dfZoneCounty[["ZONE","County_FIPS","COUNTY"]], left_on='Zone_FIPS', right_on='ZONE', how='left')
+print(dfCyclonesSummary)
+pd.reset_option('display.max_rows')
+
+
